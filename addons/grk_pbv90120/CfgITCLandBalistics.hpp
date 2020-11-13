1281da951228a9281d28dafb15f39ec3234d1ab6
Björn Dahlgren <bjorn@dahlgren.at>
2020-11-13 13:18:17 +0100

    Add GrkPbv 90120

---
+++ addons/grk_pbv90120/CfgITCLandBalistics.hpp
@@ -0,0 +1,13 @@
+class itc_land_ballistics {
+	class batteryTypes {
+		class sfp_grkpbv90120 {
+			displayName = "GrkPbv 90120 (AMOS) - 120mmm Mortar";
+			type = "cannon";
+			ammunition[] = {
+				"sfp_2Rnd_120mm_Mo_shells",
+				"sfp_2nd_120mm_Guided_shells"
+			};
+			weaponClass = "sfp_mortar_120mm";
+		};
+	};
+};
---
+++ addons/grk_pbv90120/config.cpp
@@ -0,0 +1,13 @@
+class CfgPatches {
+	class sfp_itc_land_grk_pbv90120 {
+		author = "Swedish Forces Pack + ITC Addons Team";
+		authors[] = {"Swedish Forces Pack","ToadBall","Yax"};
+		requiredaddons[] = {"A3_Weapons_F","sfp_strf90","sfp_weapons"};
+		requiredversion = 1.0;
+		units[] = {};
+		weapons[] = {};
+		magazines[] = {};
+	};
+};
+
+#include "CfgITCLandBalistics.hpp"
